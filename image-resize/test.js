d4b0d7aaf8b0e3f2462871d65e1d6faf7fc395a5
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-02-15 10:37:47 +0600

    sd

--- image-resize/test.js
+++ image-resize/test.js
@@ -6,3 +6,11 @@ writer.write('Hi There ');
 writer.write('Hi There ');
 writer.write('Hi There ');
 writer.end();
+
+fs = require('fs')
+fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
