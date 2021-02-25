5ed17db997a964744ae909d0cc40975cb15fc8bc
anisur072 <anisur.rahman@softbdltd.com>
2021-02-25 12:38:07 +0600

    fixing models

--- api-version-update/api/models/Product.js
+++ api-version-update/api/models/Product.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     price: {
       type: 'number',
-      columnType: 'float',
+      columnType: 'decimal',
       required: true,
     },
     vendor_price: {
@@ -44,22 +44,22 @@ module.exports = {
     alert_quantity: {
       type: 'number',
       columnType: 'integer',
-      required: false,
-      defaultsTo: 1
+      required: true
     },
     image: {
       type: 'string',
       columnType: 'text',
+      required: false,
       allowNull: true
     },
     rating: {
       type: 'number',
-      columnType: 'float',
+      columnType: 'double',
+      required: false,
       allowNull: true
     },
     brand_id: {
-      model: 'brand',
-      required: false,
+      model: 'brand'
     },
     type_id: {
       model: 'category',
@@ -74,8 +74,9 @@ module.exports = {
     },
     quantity: {
       type: 'number',
-      columnType: 'float',
-      required: true,
+      columnType: 'decimal',
+      required: false,
+      defaultsTo: 0
     },
     is_coupon_product: {
       type: 'number',
@@ -84,8 +85,7 @@ module.exports = {
       defaultsTo: 0
     },
     warehouse_id: {
-      model: 'warehouse',
-      required: true
+      model: 'warehouse'
     },
     product_variants: {
       collection: 'productVariant',
@@ -102,71 +102,95 @@ module.exports = {
     product_details: {
       type: 'string',
       columnType: 'text',
-      required: true,
+      required: false,
+      allowNull: true
     },
     craftsman_id: {
       model: 'user',
     },
     craftsman_price: {
       type: 'number',
-      columnType: 'decimal'
+      columnType: 'decimal',
+      required: false,
+      allowNull: true
     },
     promotion: {
       type: 'boolean',
-      columnType: 'integer'
+      columnType: 'integer',
+      required: false,
+      defaultsTo: 0
     },
     promo_price: {
       type: 'number',
-      columnType: 'decimal'
+      columnType: 'decimal',
+      required: false,
+      allowNull: true
     },
     start_date: {
       type: 'ref',
       columnType: 'datetime',
+      required: false,
+      allowNull: true
     },
     end_date: {
       type: 'ref',
       columnType: 'datetime',
+      required: false,
+      allowNull: true
     },
     sale_unit: {
       type: 'number',
       columnType: 'integer',
+      required: false,
       allowNull: true
     },
     ///end promotion
     tag: {
       type: 'string',
-      columnType: 'text'
+      columnType: 'text',
+      required: false,
+      allowNull: true
     },
     status: {
       type: 'number',
       columnType: 'integer',
+      required: true
     },
     approval_status: {
       type: 'number',
       columnType: 'integer',
+      required: true,
     },
     approval_status_updated_by: {
       type: 'number',
       columnType: 'integer',
+      required: false,
       allowNull: true
     },
     featured: {
       type: 'number',
       columnType: 'integer',
+      required: false,
+      allowNull: true
     },
     weight: {
       type: 'number',
       columnType: 'decimal',
+      required: false,
+      allowNull: true
     },
     produce_time: {
       type: 'number',
       columnType: 'integer',
+      required: false,
       allowNull: true
     },
     last_order_completed_date: {
       type: 'ref',
       columnType: 'datetime',
       columnName: 'last_order_completed_date',
+      required: false,
+      allowNull: true
     },
     updated_by: {
       model: 'user'
