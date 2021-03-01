db498386567442b17cc1646b86fe933a2826e32e
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail>
2021-03-01 18:11:33 +0600

    PartController.js is updated

--- api-version-update/api/controllers/PartController.js
+++ api-version-update/api/controllers/PartController.js
@@ -23,78 +23,61 @@ module.exports = {
       }
     }
 
-    if (req.body.hasImage === 'true') {
-      const uploadConfig = imageUploadConfig();
-      req.file('image').upload(
-        {
-          ...uploadConfig,
-          saveAs: Date.now() + '_part.jpg'
-        },
-        (err, uploaded) => {
-          if (err) {
-            return res.json(err.status, {err: err});
-          }
-          const newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
-          if (err) {
-            return res.serverError(err);
-          }
-          req.body.image = '/' + newPath;
-          create(req.body);
+    try {
+      if (req.body.hasImage === 'true') {
+        const uploadConfig = imageUploadConfig();
+        const uploaded = await req.file('image').upload({...uploadConfig, saveAs: Date.now() + '_part.jpg'});
+        if (uploaded.length === 0) {
+          return res.badRequest('No files was uploaded');
         }
-      );
-    } else {
+        let newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
+        req.body.image = '/' + newPath;
+        create(req.body);
+      }
+
       create(req.body);
+    } catch (error) {
+      console.log(error);
+      return res.json(error.status, {error: error});
     }
   },
   //Method called for updating a product part data
   //Model models/Part.js
   update: async (req, res) => {
-    if (req.body.hasImage == 'true') {
-
-      req.file('image').upload(imageUploadConfig(),
-        (err, uploaded) => {
-          if (err) {
-            return res.json(err.status, {err: err});
+    try {
+      if (req.body.hasImage === 'true') {
+        try {
+          const uploaded = await uploadImages(req.file('image'));
+          if (uploaded.length === 0) {
+            return res.badRequest('No file was uploaded');
           }
-          const newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
-          if (err) {
-            return res.serverError(err);
-          }
+          let newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
           req.body.image = '/' + newPath;
-          Part.update({id: req.param('id')}, req.body).exec((err, part) => {
-            if (err) {
-              return res.json(err.status, {err: err});
-            }
-            if (part) {
-              return res.json(200, {
-                part: part,
-                token: jwToken.issue({id: part.id})
-              });
-            }
-          });
+        } catch (error) {
+          console.log(error);
+          return res.json(error.status, {error: error});
         }
-      );
-    } else {
-      let updatedPart = await Part.update({id: req.param('id')}, req.body);
-      return res.json(200, updatedPart[0]);
+      }
+      let updatedPart = await Part.updateOne({id: req.param('id')}).set(req.body);
+      return res.json(200, updatedPart);
+
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({
+        success: false,
+        message: 'Failed to update product part',
+      });
     }
   },
 
   // destroy a row
   destroy: async (req, res) => {
-    try{
-
-    }catch (error){
+    try {
+      const part = await Part.updateOne({id: req.param('id')}).set({deletedAt: new Date()});
+      return res.json(part[0]);
+    } catch (error) {
       console.log(error);
       res.json(error.status, {error: error});
     }
-    exec(
-      (err, part) => {
-        if (err) {
-          return res.json(err, 400);
-        }
-        return res.json(part[0]);
-      }
-    );
   }
 };
