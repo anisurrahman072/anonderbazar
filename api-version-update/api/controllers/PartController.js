3619f37ceb3027c0aed7bf4be50e46c6d9750c2b
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail>
2021-03-01 16:36:28 +0600

    working on controllers and profile-tab.component.html

--- api-version-update/api/controllers/PartController.js
+++ api-version-update/api/controllers/PartController.js
@@ -1,4 +1,4 @@
-const { uploadImgAsync } = require('../../libs');
+const {uploadImgAsync} = require('../../libs');
 const {imageUploadConfig} = require('../../libs/helper');
 
 /**
@@ -12,16 +12,15 @@ let asyncForEach = require('../../libs').asyncForEach;
 module.exports = {
   //Method called for creating a product part data
   //Model models/Part.js
-  create: function(req, res) {
-    function create(body) {
-      Part.create(body).exec((err, design) => {
-        if (err) {
-          return res.json(err.status, { err: err });
-        }
-        if (design) {
-          res.json(200, design);
-        }
-      });
+  create: async (req, res) => {
+    async function create(body) {
+      try {
+        const part = await Part.create(body).fetch();
+        return res.json(200, part);
+      } catch (error) {
+        console.log(error);
+        res.json(error.status, {error: error});
+      }
     }
 
     if (req.body.hasImage === 'true') {
@@ -33,10 +32,12 @@ module.exports = {
         },
         (err, uploaded) => {
           if (err) {
-            return res.json(err.status, { err: err });
+            return res.json(err.status, {err: err});
           }
           const newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
-          if (err) {return res.serverError(err);}
+          if (err) {
+            return res.serverError(err);
+          }
           req.body.image = '/' + newPath;
           create(req.body);
         }
@@ -53,38 +54,45 @@ module.exports = {
       req.file('image').upload(imageUploadConfig(),
         (err, uploaded) => {
           if (err) {
-            return res.json(err.status, { err: err });
+            return res.json(err.status, {err: err});
           }
           const newPath = uploaded[0].fd.split(/[\\//]+/).reverse()[0];
-          if (err) {return res.serverError(err);}
+          if (err) {
+            return res.serverError(err);
+          }
           req.body.image = '/' + newPath;
-          Part.update({ id: req.param('id') }, req.body).exec((
-            err,
-            part
-          ) => {
+          Part.update({id: req.param('id')}, req.body).exec((err, part) => {
             if (err) {
-              return res.json(err.status, { err: err });
+              return res.json(err.status, {err: err});
             }
             if (part) {
               return res.json(200, {
                 part: part,
-                token: jwToken.issue({ id: part.id })
+                token: jwToken.issue({id: part.id})
               });
             }
           });
         }
       );
     } else {
-      let updatedPart = await Part.update({ id: req.param('id') }, req.body);
+      let updatedPart = await Part.update({id: req.param('id')}, req.body);
       return res.json(200, updatedPart[0]);
     }
   },
 
   // destroy a row
-  destroy: function(req, res) {
-    Part.update({ id: req.param('id') }, { deletedAt: new Date() }).exec(
+  destroy: async (req, res) => {
+    try{
+
+    }catch (error){
+      console.log(error);
+      res.json(error.status, {error: error});
+    }
+    exec(
       (err, part) => {
-        if (err) {return res.json(err, 400);}
+        if (err) {
+          return res.json(err, 400);
+        }
         return res.json(part[0]);
       }
     );
