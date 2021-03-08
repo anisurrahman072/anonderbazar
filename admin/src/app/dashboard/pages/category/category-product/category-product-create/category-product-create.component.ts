7e7081eaa67f0d7f15c95968e2d80d58318e6a53
shakibo <shakib.shaon@softbdltd.com>
2021-03-08 13:04:56 +0600

    solving table issues

--- admin/src/app/dashboard/pages/category/category-product/category-product-create/category-product-create.component.ts
+++ admin/src/app/dashboard/pages/category/category-product/category-product-create/category-product-create.component.ts
@@ -1,14 +1,14 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {
     FormBuilder,
     FormGroup,
     Validators
 } from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
-import {NzNotificationService} from 'ng-zorro-antd';
 import {CategoryProductService} from '../../../../../services/category-product.service';
 import {UploadMetadata, FileHolder} from 'angular2-image-upload';
-import { CmsService } from '../../../../../services/cms.service';
+import {CmsService} from '../../../../../services/cms.service';
+import {NzNotificationService} from "ng-zorro-antd";
 
 @Component({
     selector: 'app-category-product-create',
@@ -23,8 +23,6 @@ export class CategoryProductCreateComponent implements OnInit {
     @ViewChild('Image') Image;
     categorySearchOptions = [];
     subCategorySearchOptions = [];
-    submitting: boolean = false;
-
 
     constructor(private router: Router, private cmsService: CmsService, private route: ActivatedRoute,
                 private _notification: NzNotificationService,
@@ -35,13 +33,13 @@ export class CategoryProductCreateComponent implements OnInit {
             parent: ['', []],
             offer_id: ['', []],
             sub_parent: ['', []],
-            code:['', [Validators.required]],
+            code: ['', [Validators.required]],
             image: [null, []],
         });
     }
+
     //Event method for submitting the form
     submitForm = ($event, value) => {
-        this.submitting = true;
         $event.preventDefault();
         for (const key in this.validateForm.controls) {
             this.validateForm.controls[key].markAsDirty();
@@ -51,12 +49,12 @@ export class CategoryProductCreateComponent implements OnInit {
         formData.append('name', value.name);
         formData.append('code', value.code);
         let parent_id = 0;
-        if(value.sub_parent){
+        if (value.sub_parent) {
             parent_id = value.sub_parent.id;
-        } else if(value.parent){
+        } else if (value.parent) {
             parent_id = value.parent.id;
         }
-        formData.append('parent_id', ''+parent_id);
+        formData.append('parent_id', '' + parent_id);
 
         formData.append('type_id', '2');
         if (value.offer_id) {
@@ -75,17 +73,12 @@ export class CategoryProductCreateComponent implements OnInit {
         }
         this.categoryProductService.insert(formData)
             .subscribe((result: any) => {
-                this.submitting = false;
                 if (result.id) {
                     this._notification.create('success', 'Product category ', result.name);
                     this.router.navigate(['/dashboard/category/product/details/', result.id]);
 
                 }
-            },
-        (error: any) => {
-            this.submitting = false;
-            this._notification.create('error', 'Error occurred!', "Product Category didn't created!!");
-        });
+            });
     }
 
     //Event method for removing picture
@@ -95,11 +88,13 @@ export class CategoryProductCreateComponent implements OnInit {
             1
         );
     }
+
     //Event method for storing imgae in variable
     onBeforeUpload = (metadata: UploadMetadata) => {
         this.ImageFile.push(metadata.file);
         return metadata;
     };
+
     //Event method for resetting the form
     resetForm($event: MouseEvent) {
         $event.preventDefault();
@@ -121,13 +116,14 @@ export class CategoryProductCreateComponent implements OnInit {
             this.categorySearchOptions = result;
         });
         this.cmsService
-      .getAllSearch({page: 'POST', section: 'HOME', subsection: 'OFFER'})
-      .subscribe(result => {
-            this.offers = result;
-      });
+            .getAllSearch({page: 'POST', section: 'HOME', subsection: 'OFFER'})
+            .subscribe(result => {
+                this.offers = result;
+            });
     }
+
     //Event method on category change
-    onCategoryChange(categoryId){
+    onCategoryChange(categoryId) {
         this.parentCheck = false;
         this.categoryProductService.getSubcategoryByCategoryId(categoryId.id).subscribe((result: any) => {
             this.subCategorySearchOptions = result;
--- admin/src/app/dashboard/pages/category/category-type/category-type/category-type.component.ts
+++ admin/src/app/dashboard/pages/category/category-type/category-type/category-type.component.ts
@@ -1,102 +1,111 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoryTypeService } from '../../../../../services/category-type.service';
-
-import { NzNotificationService } from 'ng-zorro-antd';
+import {Component, OnInit} from '@angular/core';
+import {CategoryTypeService} from '../../../../../services/category-type.service';
 import {environment} from "../../../../../../environments/environment";
+import {NzNotificationService} from 'ng-zorro-antd';
 
 @Component({
-  selector: 'app-category-type',
-  templateUrl: './category-type.component.html',
-  styleUrls: ['./category-type.component.css']
+    selector: 'app-category-type',
+    templateUrl: './category-type.component.html',
+    styleUrls: ['./category-type.component.css']
 })
 export class CategoryTypeComponent implements OnInit {
-  data = [];
-  IMAGE_ENDPOINT = environment.IMAGE_ENDPOINT;
+    data = [];
+    IMAGE_ENDPOINT = environment.IMAGE_ENDPOINT;
 
-  limit: number = 10;
-  page: number = 1;
-  total: number;
-  nameSearchValue: string = '';
-  sortValue = {
-    name: null,
-    price: null
-  };
-  categoryId: any = null;
-  subcategoryId: any = null;
-  _isSpinning = true;
-
-  subcategorySearchOptions: any;
-  categorySearchOptions: any[] = [];
-
-  constructor(
-    private categoryTypeService: CategoryTypeService,
-    private _notification: NzNotificationService
-  ) {}
-
-  ngOnInit(): void {
-    this.getPageData();
-  }
-
-  getPageData() {
-    this.categoryTypeService
-      .getAllCategories(
-        this.page,
-        this.limit,
-        this.nameSearchValue,
-        this.categoryId ? this.categoryId : '',
-        this.subcategoryId ? this.subcategoryId : '',
-        this.filterTerm(this.sortValue.name),
-        this.filterTerm(this.sortValue.price)
-      )
-      .subscribe(
-        (result: any) => {
-          this.data = result.data;
-          this.total = result.total;
-          this._isSpinning = false;
-        },
-        error => {
-          this._isSpinning = false;
-        }
-      );
-  }
-  private filterTerm(sortValue: string): string {
-    switch (sortValue) {
-      case 'ascend':
-        return 'ASC';
-      case 'descend':
-        return 'DESC';
-      default:
-        return '';
-    }
-  }
-  sort(sortName, sortValue) {
-    this.page = 1;
-    this.sortValue[sortName] = sortValue;
-    this.getPageData();
-  }
-  resetAllFilter() {
-    this.limit = 5;
-    this.page = 1;
-    this.nameSearchValue = '';
-    this.sortValue = {
-      name: null,
-      price: null
+    limit: number = 10;
+    page: number = 1;
+    total: number;
+    nameSearchValue: string = '';
+    sortValue = {
+        name: null,
+        price: null
     };
-    this.categoryId = null;
-    this.subcategoryId = null;
-    this.subcategorySearchOptions = [];
-    this.getPageData();
-  }
-  changePage(page: number, limit: number) {
-    this.page = page;
-    this.limit = limit;
-    this.getPageData();
-    return false;
-  } 
-  deleteConfirm(id) {
-    this.categoryTypeService.delete(id).subscribe(result => {
-      this._notification.create('warning', 'Delete', 'Product type has been removed successfully');
-      this.getPageData();
-    });
-  }
+    categoryId: any = null;
+    subcategoryId: any = null;
+    _isSpinning = true;
+
+    subcategorySearchOptions: any;
+    categorySearchOptions: any[] = [];
+    loading: boolean = false;
+
+    constructor(
+        private categoryTypeService: CategoryTypeService,
+        private _notification: NzNotificationService
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.getPageData();
+    }
+
+    getPageData() {
+        this.loading = true;
+        this.categoryTypeService
+            .getAllCategories(
+                this.page,
+                this.limit,
+                this.nameSearchValue,
+                this.categoryId ? this.categoryId : '',
+                this.subcategoryId ? this.subcategoryId : '',
+                this.filterTerm(this.sortValue.name),
+                this.filterTerm(this.sortValue.price)
+            )
+            .subscribe(
+                (result: any) => {
+                    this.loading = false;
+                    this.data = result.data;
+                    this.total = result.total;
+                    this._isSpinning = false;
+                },
+                error => {
+                    this._isSpinning = false;
+                    this.loading = false;
+                }
+            );
+    }
+
+    private filterTerm(sortValue: string): string {
+        switch (sortValue) {
+            case 'ascend':
+                return 'ASC';
+            case 'descend':
+                return 'DESC';
+            default:
+                return '';
+        }
+    }
+
+    sort(sort: { key: string, value: string }) {
+        this.page = 1;
+        this.sortValue[sort.key] = sort.value;
+        this.getPageData();
+    }
+
+    resetAllFilter() {
+        this.limit = 5;
+        this.page = 1;
+        this.nameSearchValue = '';
+        this.sortValue = {
+            name: null,
+            price: null
+        };
+        this.categoryId = null;
+        this.subcategoryId = null;
+        this.subcategorySearchOptions = [];
+        this.getPageData();
+    }
+
+    changePage(page: number, limit: number) {
+        this.page = page;
+        this.limit = limit;
+        this.getPageData();
+        return false;
+    }
+
+    deleteConfirm(id) {
+        this.categoryTypeService.delete(id).subscribe(result => {
+            this._notification.create('warning', 'Delete', 'Product type has been removed successfully');
+            this.getPageData();
+        });
+    }
 }
