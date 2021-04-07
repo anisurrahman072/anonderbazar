34ff1305876e0e2a90a9d60cc0eb661bccc8cc44
anisur072 <anisur.rahman@softbdltd.com>
2021-04-07 17:23:31 +0600

    Added brand ordering in front-end

--- web/src/app/modules/home/components/section-brand/section-brand.component.ts
+++ web/src/app/modules/home/components/section-brand/section-brand.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {BrandService, ProductService} from "../../../../services";
+import {concatMap, debounceTime} from 'rxjs/operators';
+import {from} from "rxjs/observable/from";
+import {error} from "util";
+
 
 @Component({
     selector: 'app-section-brand',
@@ -8,7 +12,7 @@ import {BrandService, ProductService} from "../../../../services";
 })
 export class SectionBrandComponent implements OnInit {
 
-    dataBrandList: any = null;
+    dataBrandList: any = [];
     IMAGE_ENDPOINT = 'https://anonderbazar.s3-ap-southeast-1.amazonaws.com';
 
     constructor(private brandService: BrandService,
@@ -16,19 +20,24 @@ export class SectionBrandComponent implements OnInit {
     }
 
     ngOnInit() {
+        let allBrands;
         this.brandService.getAll(true)
-            .subscribe(brands => {
-                brands.map(brand => {
-                    this.productService.getAllByBrandId(brand.id)
-                        .subscribe(products => {
-                            if(products.length > 0){
-                                if(!this.dataBrandList) this.dataBrandList = [];
-                                if(this.dataBrandList.length < 8) this.dataBrandList.push(brand);
-                            }
-                        });
-                });
-            }, error => {
-                console.log('Error occurred');
-            });
+            .subscribe((brands) =>{
+                allBrands = brands;
+
+                let currentBrand;
+                from(allBrands)
+                    .concatMap((brand:any) => {
+                        currentBrand = brand;
+                        return this.productService.getAllByBrandId(brand.id)
+                    })
+                    .subscribe(products => {
+                        if(products.length !== 0){
+                            if(!this.dataBrandList) this.dataBrandList = [];
+                            if(this.dataBrandList.length < 8) this.dataBrandList.push(currentBrand);
+                        }}, error => {
+                        console.log('Error occurred', error);
+                    })
+            })
     }
 }
