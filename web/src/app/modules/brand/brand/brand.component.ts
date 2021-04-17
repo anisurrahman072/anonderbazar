520642c9ac306c42f93afc1ddc3e0eff7eba1e18
anisur072 <anisur.rahman@softbdltd.com>
2021-04-17 12:07:40 +0600

    Fixed issue

--- web/src/app/modules/brand/brand/brand.component.ts
+++ web/src/app/modules/brand/brand/brand.component.ts
@@ -1,42 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BrandService, ProductService} from "../../../services";
 import {AppSettings} from "../../../config/app.config";
+import * as ___ from 'lodash';
 
 @Component({
-  selector: 'app-brand',
-  templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.scss'],
+    selector: 'app-brand',
+    templateUrl: './brand.component.html',
+    styleUrls: ['./brand.component.scss'],
 })
 export class BrandComponent implements OnInit {
 
-  dataBrandList: any = [];
-  IMAGE_ENDPOINT = AppSettings.IMAGE_ENDPOINT;
+    dataBrandList: any = [];
+    IMAGE_ENDPOINT = AppSettings.IMAGE_ENDPOINT;
 
-  constructor(private brandService: BrandService,
-              private productService: ProductService) {
-  }
+    constructor(private brandService: BrandService,
+                private productService: ProductService) {
+    }
 
-  ngOnInit() {
-    let allBrands;
-    this.brandService.getAll(true)
-        .subscribe((brands) =>{
-          allBrands = brands;
+    ngOnInit() {
+        this.brandService.getAll(true)
+            .concatMap((brands) => {
+                this.dataBrandList = brands;
 
-          let allBrandIds = allBrands.map(brand => {
-            return brand.id;
-          });
-
-          this.productService.getCountByBrandIds(allBrandIds)
-              .subscribe((counts) => {
-                this.dataBrandList = counts.data.map((count, index) => {
-                  if(count){
-                    return allBrands[index];
-                  }
-                }).filter(data => data);
-              }, error => {
-                  console.log('Error occurred while fetching brands', error);
-              });
-        })
-  }
+                let allBrandIds = this.dataBrandList.map(brand => {
+                    return brand.id;
+                });
+                return this.productService.getCountByBrandIds(allBrandIds);
+            })
+            .subscribe((counts) => {
+                let brandCounts = counts.data;
+                if(!___.isEmpty(brandCounts)){
+                    this.dataBrandList = this.dataBrandList.filter((brand:any) => {
+                        return !___.isUndefined(brandCounts[brand.id]);
+                    })
+                }
+            }, error => {
+                console.log('Error occurred while fetching brands', error);
+            });
+    }
 
 }
