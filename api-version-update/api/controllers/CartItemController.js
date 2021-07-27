ff0a647e6eb8dd89e788d45929eda8e658139a26
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-07-27 11:52:45 +0600

    Code optimization in offer section + offer payment gateway adding & multiple offer products can't be added in cart 65% done

--- api-version-update/api/controllers/CartItemController.js
+++ api-version-update/api/controllers/CartItemController.js
@@ -176,9 +176,54 @@ module.exports = {
         return res.badRequest('Product stock is not sufficient enough.');
       }
 */
+      let offeredProducts = await OfferService.getAllOfferedProducts();
+
+      let cartItems = await CartItem.find({
+        cart_id: req.body.cart_id,
+        product_id: req.body.product_id,
+        deletedAt: null
+      }).populate('product_id');
+
+      let offerIdNumber;
+      let offerType;
+
+      for(let i=0; i < cartItems.length; i++){
+        let {offer_id_number, offer_type} = await OfferService.getProductOfferInfo({
+          id: cartItems[i].product_id.id,
+          type_id: cartItems[i].product_id.type_id,
+          category_id: cartItems[i].product_id.category_id,
+          subcategory_id: cartItems[i].product_id.subcategory_id,
+          brand_id: cartItems[i].product_id.brand_id,
+          warehouse_id: cartItems[i].product_id.warehouse_id
+        }, offeredProducts);
+
+        if(i > 0){
+          if(offerIdNumber !== offer_id_number || offerType !== offer_type){
+            console.log('Asessse111');
+            return res.badRequest('Different offer products or an offer product with regular a product can\'t be added at a time in your cart!');
+          }
+        }
+
+        offerIdNumber = offer_id_number;
+        offerType = offer_type;
+      }
+
+      let {offer_id_number, offer_type} = await OfferService.getProductOfferInfo({
+        id: product.id,
+        type_id: product.type_id,
+        category_id: product.category_id,
+        subcategory_id: product.subcategory_id,
+        brand_id: product.brand_id,
+        warehouse_id: product.warehouse_id
+      }, offeredProducts);
+
+      if(cartItems && cartItems.length > 0 && (offerIdNumber !== offer_id_number || offerType !== offer_type)){
+        console.log('Asessse222');
+        return res.badRequest('Different offer products or an offer product with regular a product can\'t be added together in your cart!');
+      }
 
       let offerInfo = req.body.offerInfo;
-      console.log('changed offerInfo: cccc', offerInfo);
+      console.log('changed offerInfo: ', offerInfo);
 
       let productUnitPrice = product.price;
 
@@ -190,11 +235,7 @@ module.exports = {
         }
       }
 
-      let cartItems = await CartItem.find({
-        cart_id: req.body.cart_id,
-        product_id: req.body.product_id,
-        deletedAt: null
-      });
+
 
 
       let selectedCartItem = null;
@@ -210,7 +251,7 @@ module.exports = {
               if (!(selectedCartItem !== null && cartItemVariants.length === (i + 1) && cartItemVariantsLength < 1)) {
                 let cartItemVar = cartItemVariants[i];
                 cartItemVar.cart_item_id = cartItems[h].id;
-                cartItemVar.product_id = cartItems[h].product_id;
+                cartItemVar.product_id = cartItems[h].product_id.id;
                 let cartvariant = await CartItemVariant.find(cartItemVar);
 
                 if (cartvariant && cartvariant.length > 0 && cartvariant[0]) {
