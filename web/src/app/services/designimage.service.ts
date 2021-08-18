b1dde610452dc028245e3f8dff98dc7225995430
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-08-18 18:13:37 +0600

    Done Excel Sheet (August) No: 17

--- web/src/app/services/designimage.service.ts
+++ web/src/app/services/designimage.service.ts
@@ -10,13 +10,22 @@ import { HttpClient } from "@angular/common/http";
 export class DesignimageService {
   private EndPoint = `${AppSettings.API_ENDPOINT}/designimage`;
   private EndPoint2 = `${AppSettings.API_ENDPOINT}/designimages`;
+  private EndPoint3 = `${AppSettings.API_ENDPOINT}/image`;
   constructor(
     private http: HttpClient,
     private authenticationService: AuthService
   ) { }
 
-  getImageWithCombination(combination:string,product_id:number):Observable<any>{ 
-    return this.http.get(`${this.EndPoint2}/getSingleByProductId/${product_id}?combination=${combination}`) 
+  getImageWithCombination(combination:string,product_id:number):Observable<any>{
+    return this.http.get(`${this.EndPoint2}/getSingleByProductId/${product_id}?combination=${combination}`)
+  }
+
+  insertImage(data): Observable<any> {
+    return this.http.put(`${this.EndPoint3}/insert-image`, data);
+  }
+
+  deleteImage(data): Observable<any> {
+    return this.http.put(`${this.EndPoint3}/delete-image`, data);
   }
 
 }
