5c6da2879bec1482adfe00d3c908db3e1892887a
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-08-25 12:13:16 +0600

    Done logger for error by using winston

--- api-version-update/config/log.js
+++ api-version-update/config/log.js
@@ -9,6 +9,54 @@
  * For more information on the Sails logger, check out:
  * https://sailsjs.com/docs/concepts/logging
  */
+let {transports, createLogger, format} = require('winston');
+let { combine, timestamp, label, prettyPrint } = format;
+
+let commonSettings = {
+  colorize: false,
+  format: combine(
+    label({ label: `Anonderbazar Error occurred!` }),
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    prettyPrint()
+  ),
+  json: true
+};
+
+let settings = [
+  {filename: `${__dirname}/../logs/warn.log`, level: 'warn'},
+  {filename: `${__dirname}/../logs/error.log`, level: 'error'},
+  {filename: `${__dirname}/../logs/debug.log`, level: 'debug'},
+  {filename: `${__dirname}/../logs/info.log`, level: 'info'}
+];
+
+let loggerSettings = {
+  transports: [...settings.map(s => new transports.File({...s, ...commonSettings})), new transports.Console({
+    format: format.simple()
+  })],
+  exitOnError: false
+};
+
+// This is the workaround
+let winstonLogger = createLogger(loggerSettings);
+let logger = {
+  'info': function () {
+    winstonLogger.info(...arguments);
+  },
+  'debug': function () {
+    winstonLogger.debug(...arguments);
+  },
+  'error': function () {
+    winstonLogger.error(...arguments);
+  },
+  'warn': function () {
+    winstonLogger.warn(...arguments);
+  },
+  'log': function () {
+    winstonLogger.log(...arguments);
+  }
+};
 
 module.exports.log = {
 
@@ -23,7 +71,8 @@ module.exports.log = {
   * You may also set the level to "silent" to suppress all logs.             *
   *                                                                          *
   ***************************************************************************/
+  custom: logger,
+  inspect: false
 
-   level: 'debug'
-
+  /*level: 'debug'*/
 };
